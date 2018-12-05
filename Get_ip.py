528d1899be0c4f68a3574810497cbc2345144a26
Pluto00 <1834733586@qq.com>
2018-12-05 23:08:50 +0800

    IP代理部分完成

--- Get_ip.py
+++ Get_ip.py
@@ -1,42 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
+import telnetlib
 
 
-def get_ip_list(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-                      '(KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134'
-    }
-
-    html = requests.get(url, headers=headers)
-
-    soup = BeautifulSoup(html.text, 'html.parser')
-
-    trs = soup.find_all('tr')
-
-    ip_list = []
-
-    for tr in trs:
-        try:
-
-            tds = tr.find_all('td')
-
-            ip_list.append(tds[1].text + ':' + tds[2].text)
-
-        except:
-            pass
-
-    return ip_list
-
-
-if __name__ == '__main__':
-
-    url = 'http://www.xicidaili.com/wn/'
-
-    ip_list = get_ip_list(url)
-
-    for ip in ip_list:
-        proxies = {
-            'https': "https://" + ip
+class IP(object):
+    def __init__(self):
+        self.url = 'http://www.xicidaili.com/wn/'
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                          '(KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134'
         }
-        print(proxies)
+        self.ip_list = []
+        self.ip_proxies = []
+        self.get_ip_proxies()
+
+    def get_ip_proxies(self):
+        html = requests.get(url=self.url, headers=self.headers)
+        soup = BeautifulSoup(html.text, 'html.parser')
+        trs = soup.find_all('tr')
+        for tr in trs:
+            try:
+                tds = tr.find_all('td')
+                # 连接Telnet服务器测试ip是否可用
+                telnetlib.Telnet(tds[1].text, port=tds[2].text, timeout=5)
+            except:
+                pass
+            else:
+                self.ip_list.append(tds[1].text + ':' + tds[2].text)
+        for ip in self.ip_list:
+            proxies = {
+                'https': "https://" + ip
+            }
+            self.ip_proxies.append(proxies)
+        return self.ip_proxies
