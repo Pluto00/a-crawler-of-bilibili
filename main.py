3453bcf4ea17bb7a97050481b3850433c14a3441
Pluto00 <1834733586@qq.com>
2018-12-08 16:37:57 +0800

    update

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ class GetInfo(object):  # 定义一个爬取类
             'Origin': 'https://www.bilibili.com',
         }
         self.URL_VIDinfo = "http://api.bilibili.com/archive_stat/stat?aid="  # b站视频信息api
+        self.count = 0
+        self.start_time = time.time()
         # 数据库准备
         self.db = pymysql.connect("localhost", "root", "", "bilibili", charset='utf8')  # 连接数据库
         self.cursor = self.db.cursor()  # 获得操作游标
@@ -48,31 +50,37 @@ class GetInfo(object):  # 定义一个爬取类
                     if mutex.acquire(True):
                         for row in video_list:
                             self.cursor.execute(self.sql, row)
-                            print(row)
+                            # print(row)
+                            self.count += 1
                     mutex.release()
                 except Exception as e:
                     print(e)
                 else:  # 降低爬虫速度，防止被禁
+                    if self.count > 0 and self.count % 100 == 0:
+                        print("爬取平均速度：%d 部/秒" % (self.count / (time.time() - self.start_time)))
                     time.sleep(1)
 
 
 if __name__ == '__main__':
-    # 初始化 获得
-    video = GetInfo()  # 初始化爬取类
-    thread = []
+    for j in range(10):
+        print("开始第 %d 轮循环爬取" % j)
+        # 初始化获得ip
+        video = GetInfo()  # 初始化爬取类
+        print("开始爬取信息.....")
+        # 设置多线程
+        thread = []
+        mutex = threading.Lock()  # 设置互斥锁，保护线程安全
+        for i in range(j*10000, j*10000+10000, 1000):  # 设置爬虫起点和结束的av号，根据需求自己设置
+            t = threading.Thread(target=video.download, args=(i, i + 1000))
+            thread.append(t)
+        for i in range(len(thread)):
+            thread[i].start()
+        for i in range(len(thread)):
+            thread[i].join()
 
-    # 设置多线程
-    mutex = threading.Lock()  # 设置互斥锁，保护线程安全
-    for i in range(1, 1000, 100):  # 设置爬虫起点和结束的av号，根据需求自己设置
-        t = threading.Thread(target=video.download, args=(i, i + 100))
-        thread.append(t)
-    for i in range(len(thread)):
-        thread[i].start()
-    for i in range(len(thread)):
-        thread[i].join()
-
-    # mysql操作
-    video.cursor.close()  # 关闭游标
-    video.db.commit()  # 提交之前的操作,否则数据不会插进去，是个巨坑
-    video.db.close()  # 关闭数据库
-    print("程序结束！！")
+        # mysql操作
+        video.cursor.close()  # 关闭游标
+        video.db.commit()  # 提交之前的操作,否则数据不会插进去，是个巨坑
+        video.db.close()  # 关闭数据库
+        print("第 %d 轮循环功爬取 %d 部视频" % (j, video.count))
+    print("程序结束~~~~~~")
